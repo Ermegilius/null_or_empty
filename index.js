4d8d2af835aadce6b03773319ef88f10099a495e
Beliakov Vladimir <ermegilius@gmail.com>
2025-01-14 13:04:43 +0200

    init commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Main package function
 function isNullOrEmpty(input) {
+
     // Returns true if the input is either undefined, null, or empty, false otherwise
     return (input === undefined || input === null || input === '');
 }
